cbbf1e660306f3577cdc351e497ecd5df915bcd6
Pedro Santos <pedroguimaraes147@gmail.com>
2026-09-23 16:56:10 -0300

    V1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pedro.finances"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "v1.0.2"
+        versionCode = 4
+        versionName = "v1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
